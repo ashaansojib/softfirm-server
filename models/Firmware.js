b7171c9e63344ce54283db455f570a2cf9231338
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-08-30 02:35:43 +0600

    update firmware

---
+++ models/Firmware.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const FirmwareSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please add title"],
+  },
+  brand: {
+    type: String,
+    required: [true, "Please add brand name"],
+  },
+  model: {
+    type: String,
+    required: [true, "Please add model"],
+  },
+  chipset: {
+    type: String,
+    required: [true, "Please add chipset"],
+  },
+  size: {
+    type: String,
+    required: [true, "Please add file size"],
+  },
+  status: {
+    type: String,
+  },
+  price: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
+  createAt: {
+    type: new Date(),
+  },
+  filename: {
+    type: String,
+  },
+});
+module.exports = mongoose.model("Firmware-Schema", FirmwareSchema);
+// title, brand, name, chipset, size, price, status, link, category, date, filename
